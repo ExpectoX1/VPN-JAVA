d617364b5a75d6a41d4b5248afccba67313f92e7
Shreyansh Srivastava <88975311+shreyanshx22@users.noreply.github.com>
2023-04-20 14:08:28 +0530

    main ui page

--- MainGUI.java
+++ MainGUI.java
@@ -1,70 +1,53 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
-public class MainUI extends JFrame implements ActionListener {
-
-	private JButton	m_btnClient;
-	private JButton m_btnServer;
-	
-	public MainUI()
-	{
-		super("Title");
-		this.setSize(300,300);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new FlowLayout());
-		this.setVisible(true);
-
-		m_btnClient = new JButton("Client");
-		m_btnServer = new JButton("Server");
-		
-		m_btnClient.addActionListener(this);
-		m_btnServer.addActionListener(this);
-		
-		this.add(m_btnClient);
-		this.add(m_btnServer);
-		displayClient();
-	}
-	
-	public void actionPerformed( ActionEvent e )
-	{
-		if(e.getSource() == m_btnClient) displayClient();
-		if(e.getSource() == m_btnServer) displayServer();
-	}
-	
-	private void displayClient()
-	{
-		SwingUtilities.invokeLater( new Runnable(){
-			public void run() {
-				new ClientUI(); 
-			}
-		});
-	}
-	
-	private void displayServer()
-	{
-		SwingUtilities.invokeLater( new Runnable(){
-			public void run() {
-				new ServerUI(); 
-			}
-			
-		});
-	}
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	
-		SwingUtilities.invokeLater( new Runnable(){
-			public void run() {
-				new MainUI(); 
-			}
-			
-		});
-	}
+public class ClientServerGUI implements ActionListener {
+    private JFrame frame;
+    private JButton clientButton;
+    private JButton serverButton;
+    
+    public ClientServerGUI() {
+        frame = new JFrame("Client/Server GUI");
+        clientButton = new JButton("Client");
+        serverButton = new JButton("Server");
+        
+        clientButton.addActionListener(this);
+        serverButton.addActionListener(this);
+        
+        JPanel panel = new JPanel();
+        panel.add(clientButton);
+        panel.add(serverButton);
+        
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == clientButton) {
+            openClientGUI();
+        } else if (e.getSource() == serverButton) {
+            openServerGUI();
+        }
+    }
+    
+    private void openClientGUI() {
+        JFrame clientFrame = new JFrame("Client GUI");
+        // Add components to clientFrame here...
+        clientFrame.pack();
+        clientFrame.setVisible(true);
+    }
+    
+    private void openServerGUI() {
+        JFrame serverFrame = new JFrame("Server GUI");
+        // Add components to serverFrame here...
+        serverFrame.pack();
+        serverFrame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new ClientServerGUI();
+    }
 }
